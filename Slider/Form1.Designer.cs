46613d53d4ff30e85d1176f99b96c8c0d4595d34
ishak saad <ishakprogramingismylife@gmail.com>
2021-04-25 18:53:00 +0200

    first

---
+++ Slider/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Slider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.uploadImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onePictureModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.slideShowModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiPicturesModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showSideNavBarAsImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minmizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sideNavBarAsImagesPanel = new System.Windows.Forms.Panel();
+            this.sideNavBarAsTextListBox = new System.Windows.Forms.ListBox();
+            this.imageModesContainerPanel = new System.Windows.Forms.Panel();
+            this.imageName = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.AllowDrop = true;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uploadImagesToolStripMenuItem,
+            this.modesToolStripMenuItem,
+            this.showSideNavBarAsImagesToolStripMenuItem,
+            this.minmizeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowCheckMargin = true;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(251, 124);
+            // 
+            // uploadImagesToolStripMenuItem
+            // 
+            this.uploadImagesToolStripMenuItem.Name = "uploadImagesToolStripMenuItem";
+            this.uploadImagesToolStripMenuItem.Size = new System.Drawing.Size(250, 24);
+            this.uploadImagesToolStripMenuItem.Text = "Upload images";
+            this.uploadImagesToolStripMenuItem.Click += new System.EventHandler(this.uploadImagesToolStripMenuItem_Click);
+            // 
+            // modesToolStripMenuItem
+            // 
+            this.modesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.onePictureModeToolStripMenuItem,
+            this.slideShowModeToolStripMenuItem,
+            this.multiPicturesModeToolStripMenuItem});
+            this.modesToolStripMenuItem.Name = "modesToolStripMenuItem";
+            this.modesToolStripMenuItem.Size = new System.Drawing.Size(250, 24);
+            this.modesToolStripMenuItem.Text = "Modes";
+            // 
+            // onePictureModeToolStripMenuItem
+            // 
+            this.onePictureModeToolStripMenuItem.Name = "onePictureModeToolStripMenuItem";
+            this.onePictureModeToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.onePictureModeToolStripMenuItem.Text = "One picture mode";
+            this.onePictureModeToolStripMenuItem.Click += new System.EventHandler(this.onePictureModeToolStripMenuItem_Click);
+            // 
+            // slideShowModeToolStripMenuItem
+            // 
+            this.slideShowModeToolStripMenuItem.Name = "slideShowModeToolStripMenuItem";
+            this.slideShowModeToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.slideShowModeToolStripMenuItem.Text = "Slide show mode";
+            this.slideShowModeToolStripMenuItem.Click += new System.EventHandler(this.slideShowModeToolStripMenuItem_Click);
+            // 
+            // multiPicturesModeToolStripMenuItem
+            // 
+            this.multiPicturesModeToolStripMenuItem.Name = "multiPicturesModeToolStripMenuItem";
+            this.multiPicturesModeToolStripMenuItem.Size = new System.Drawing.Size(225, 26);
+            this.multiPicturesModeToolStripMenuItem.Text = "Multi pictures mode";
+            this.multiPicturesModeToolStripMenuItem.Click += new System.EventHandler(this.multiPicturesModeToolStripMenuItem_Click);
+            // 
+            // showSideNavBarAsImagesToolStripMenuItem
+            // 
+            this.showSideNavBarAsImagesToolStripMenuItem.CheckOnClick = true;
+            this.showSideNavBarAsImagesToolStripMenuItem.Name = "showSideNavBarAsImagesToolStripMenuItem";
+            this.showSideNavBarAsImagesToolStripMenuItem.Size = new System.Drawing.Size(250, 24);
+            this.showSideNavBarAsImagesToolStripMenuItem.Text = "side nav bar as images";
+            this.showSideNavBarAsImagesToolStripMenuItem.Click += new System.EventHandler(this.showSideNavBarAsImagesToolStripMenuItem_Click);
+            // 
+            // minmizeToolStripMenuItem
+            // 
+            this.minmizeToolStripMenuItem.Name = "minmizeToolStripMenuItem";
+            this.minmizeToolStripMenuItem.Size = new System.Drawing.Size(250, 24);
+            this.minmizeToolStripMenuItem.Text = "Minimize";
+            this.minmizeToolStripMenuItem.Click += new System.EventHandler(this.minmizeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(250, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // sideNavBarAsImagesPanel
+            // 
+            this.sideNavBarAsImagesPanel.AutoScroll = true;
+            this.sideNavBarAsImagesPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.sideNavBarAsImagesPanel.Location = new System.Drawing.Point(1300, 0);
+            this.sideNavBarAsImagesPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sideNavBarAsImagesPanel.Name = "sideNavBarAsImagesPanel";
+            this.sideNavBarAsImagesPanel.Size = new System.Drawing.Size(0, 0);
+            this.sideNavBarAsImagesPanel.TabIndex = 5;
+            this.sideNavBarAsImagesPanel.Visible = false;
+            // 
+            // sideNavBarAsTextListBox
+            // 
+            this.sideNavBarAsTextListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(153)))), ((int)(((byte)(255)))));
+            this.sideNavBarAsTextListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.sideNavBarAsTextListBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.sideNavBarAsTextListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sideNavBarAsTextListBox.FormattingEnabled = true;
+            this.sideNavBarAsTextListBox.HorizontalScrollbar = true;
+            this.sideNavBarAsTextListBox.ItemHeight = 16;
+            this.sideNavBarAsTextListBox.Location = new System.Drawing.Point(0, 0);
+            this.sideNavBarAsTextListBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.sideNavBarAsTextListBox.Name = "sideNavBarAsTextListBox";
+            this.sideNavBarAsTextListBox.Size = new System.Drawing.Size(300, 820);
+            this.sideNavBarAsTextListBox.TabIndex = 5;
+            this.sideNavBarAsTextListBox.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.sideNavBarAsTextListBox_DrawItem);
+            this.sideNavBarAsTextListBox.SelectedIndexChanged += new System.EventHandler(this.sideNavBarAsTextListBox_SelectedIndexChanged);
+            // 
+            // imageModesContainerPanel
+            // 
+            this.imageModesContainerPanel.AutoScroll = true;
+            this.imageModesContainerPanel.Location = new System.Drawing.Point(360, 100);
+            this.imageModesContainerPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.imageModesContainerPanel.Name = "imageModesContainerPanel";
+            this.imageModesContainerPanel.Size = new System.Drawing.Size(1243, 594);
+            this.imageModesContainerPanel.TabIndex = 7;
+            // 
+            // imageName
+            // 
+            this.imageName.AutoSize = true;
+            this.imageName.Location = new System.Drawing.Point(885, 34);
+            this.imageName.Name = "imageName";
+            this.imageName.Size = new System.Drawing.Size(0, 17);
+            this.imageName.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1549, 805);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.imageName);
+            this.Controls.Add(this.imageModesContainerPanel);
+            this.Controls.Add(this.sideNavBarAsTextListBox);
+            this.Controls.Add(this.sideNavBarAsImagesPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(1549, 805);
+            this.MinimumSize = new System.Drawing.Size(1533, 805);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem uploadImagesToolStripMenuItem;
+        private ToolStripMenuItem modesToolStripMenuItem;
+        private ToolStripMenuItem onePictureModeToolStripMenuItem;
+        private ToolStripMenuItem slideShowModeToolStripMenuItem;
+        private ToolStripMenuItem multiPicturesModeToolStripMenuItem;
+        private ToolStripMenuItem showSideNavBarAsImagesToolStripMenuItem;
+        private Panel sideNavBarAsImagesPanel;
+        private ListBox sideNavBarAsTextListBox;
+        private ToolStripMenuItem minmizeToolStripMenuItem;
+        private Panel imageModesContainerPanel;
+        private Label imageName;
+    }
+}
+
---
+++ Slider/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Slider
+{
+    public partial class Form1 : Form
+    {
+        bool isImage = false;
+        string[] files;
+        int imageIndex = 0;
+        string mode = "single";
+        List<string> pictuersContainer = new List<string>();
+        System.Windows.Forms.Timer SlideTimer = new System.Windows.Forms.Timer();
+       
+        OpenFileDialog opnfd = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            SlideTimer.Interval = 1500;
+            SlideTimer.Tick += SlideTimer_Tick;
+        }
+
+
+        private bool handleAddAndDeleteImageToContainer(string imagePath)
+        {
+            int indexOfImage = pictuersContainer.IndexOf(imagePath);
+            if (indexOfImage  == -1) { 
+                pictuersContainer.Add(imagePath);
+                return true;
+            }
+            else
+            {
+                pictuersContainer.RemoveAt(indexOfImage);
+                return false;
+            }
+
+        }
+
+        
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+        //////////////  handlImageSizeAndStyle start /////////////
+        #region
+        private TreeView HandlImageSizeAndStyle(int maxViewWidth,int maxViewHeight ,string imagePath , int x = 0, int y = 0)
+        {
+
+                PictureBox pic = new PictureBox();
+                pic.Image = Image.FromFile(imagePath);
+                int Height = pic.Image.Height;
+                int Width = pic.Image.Width;
+                int maxWidth;
+                int maxHeight;
+            TreeView view = new TreeView
+            {
+                Size = new Size(maxViewWidth, maxViewHeight),
+                Location = new Point(x, y)
+            };
+            if (Height > Width)
+                {
+                    maxHeight = maxViewHeight;
+                    maxWidth = (Width * maxViewHeight) / Height;
+                }
+                else
+                {
+                    maxWidth = maxViewWidth;
+                    maxHeight = (maxViewWidth * Height) / Width;
+                }
+                pic.Size = new Size(maxWidth, maxHeight);
+                int imageX = (maxViewWidth - maxWidth) / 2,
+                       imageY = (maxViewHeight - maxHeight) / 2;
+                pic.Location = new Point(imageX, imageY);
+                pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                view.Controls.Add(pic);
+                return view;
+           
+        }
+        #endregion
+        //////////////  handlImageSizeAndStyle start /////////////
+        
+        
+        //////////////  handleShowMode /////////////
+        #region
+        private void HandleShowMode()
+
+        {
+            
+            if (mode == "single" )
+            {
+                sideNavBarAsTextListBox.SelectionMode = SelectionMode.One;
+                SlideTimer.Enabled = false;
+                imageModesContainerPanel.Controls.Clear();
+                string singleImagePath =  sideNavBarAsTextListBox.GetItemText(sideNavBarAsTextListBox.SelectedItem);
+                try
+                {
+                    TreeView View = HandlImageSizeAndStyle(850, 480, singleImagePath);
+                    imageModesContainerPanel.Controls.Add(View);
+                }
+                catch
+                {
+                    MessageBox.Show("please upload the images and select one first");
+                }
+                
+            }
+            else if (mode == "multi")
+            {
+                sideNavBarAsTextListBox.SelectionMode = SelectionMode.MultiSimple;
+                SlideTimer.Enabled = false;
+                int y = 17;
+                int x = 0;
+                int margin = 18;
+                imageModesContainerPanel.Controls.Clear();
+                foreach (var selectedItem in sideNavBarAsTextListBox.SelectedItems)
+                {
+                    string oneFile = sideNavBarAsTextListBox.GetItemText(selectedItem);
+                    TreeView View = HandlImageSizeAndStyle(200, 113, oneFile , x, y);
+                    if ((sideNavBarAsTextListBox.SelectedItems.IndexOf(selectedItem) + 1) % 4 == 0)
+                    {
+                        x = 0;
+                        y += 113 + margin;
+                    }
+                    else
+                    {
+                        x += 200 + margin;
+                    }
+                    imageModesContainerPanel.Controls.Add(View);
+                }
+            }
+            else if (mode == "slideShow")
+            {
+                sideNavBarAsTextListBox.SelectionMode = SelectionMode.MultiSimple;
+                SlideTimer.Enabled = true;
+               
+            }
+            else
+            {
+                mode = "single";
+            }
+           
+        }
+        #endregion
+        /////////////// end of handleShowMode ///////////
+        private void SlideTimer_Tick(object sender, EventArgs e)
+        {
+            int length = sideNavBarAsTextListBox.SelectedItems.Count;
+            if(length == 0)
+            {
+                return;
+            }
+            if (imageIndex >= length)
+            {
+                mode = "single";
+                return;
+            }
+            if (length > 0)
+            {
+                string singleImagePath = sideNavBarAsTextListBox.GetItemText(sideNavBarAsTextListBox.SelectedItems[imageIndex]);
+                imageName.Text = singleImagePath;
+                int width = imageName.Size.Width;
+                int xAxis = 465 - width / 2 + 220;
+                imageName.Location = new Point(xAxis, 50);
+                imageIndex++;
+                imageModesContainerPanel.Controls.Clear();
+                TreeView View = HandlImageSizeAndStyle(850, 480, singleImagePath);
+                imageModesContainerPanel.Controls.Add(View);
+            }
+        }
+
+        private void HandleShowImagesBarState()
+        {
+             files = opnfd.FileNames;
+
+            if (isImage)
+            {
+                int y = 0;
+                sideNavBarAsImagesPanel.Controls.Clear();
+                foreach (string oneFile in files)
+                {
+                    TreeView View = HandlImageSizeAndStyle(200, 113, oneFile ,0, y);
+                    
+                        y += 113;
+                        sideNavBarAsImagesPanel.Controls.Add(View);
+                    
+                  }
+            }
+            else
+            {
+                sideNavBarAsTextListBox.Items.Clear();
+                foreach (string oneFile in files)
+                {
+                    sideNavBarAsTextListBox.Items.Add(oneFile);
+                }
+
+            }
+        }
+
+
+       private void HandleViewSize()
+        {
+
+            if (isImage)
+            {
+                /// sideNavBarAsImagesPanel
+                sideNavBarAsImagesPanel.Location = new Point(0, 0);
+                sideNavBarAsImagesPanel.Size = new Size(220, 655);
+
+                /// sideNavBarAsTextListBox
+                sideNavBarAsTextListBox.Location = new Point(1300, 0);
+                sideNavBarAsTextListBox.Size = new Size(0, 0);
+                sideNavBarAsTextListBox.Items.Clear();
+            }
+            else
+            {
+                /// sideNavBarAsImagesPanel
+                sideNavBarAsImagesPanel.Location = new Point(1300, 0);
+                sideNavBarAsImagesPanel.Size = new Size(0, 0);
+                sideNavBarAsImagesPanel.Controls.Clear();
+
+                /// sideNavBarAsTextListBox
+                sideNavBarAsTextListBox.Location = new Point(0, 0);
+                sideNavBarAsTextListBox.Size = new Size(220, 660);
+            }
+
+        }
+
+        /// Uploading images to the program in array of strings
+        private void uploadImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictuersContainer.Clear();
+            opnfd.Multiselect = true;
+            opnfd.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                HandleShowImagesBarState();
+            }
+        }
+
+        private void showSideNavBarAsImagesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isImage = !isImage;
+            sideNavBarAsImagesPanel.Visible = isImage;
+            sideNavBarAsTextListBox.Visible = !isImage;
+            HandleShowImagesBarState();
+            HandleViewSize();
+        }
+
+        private void minmizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void sideNavBarAsTextListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SlideTimer.Enabled = false;
+            if (sideNavBarAsTextListBox.SelectedItems.Count > 0)
+                Console.WriteLine(sideNavBarAsTextListBox.SelectedItems[0]);
+            string text = sideNavBarAsTextListBox.GetItemText(sideNavBarAsTextListBox.SelectedItem);
+            imageName.Text = text;
+            int width = imageName.Size.Width;
+            int xAxis = 465 - width / 2 + 220;  
+            imageName.Location = new Point(xAxis, 50);
+            if ( mode == "multi" || mode == "slideShow") {
+                handleAddAndDeleteImageToContainer(text);
+            }
+            else
+            { 
+                HandleShowMode();
+            }
+        }
+
+       
+
+       
+
+        private void multiPicturesModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            mode = "multi";
+            HandleShowMode();
+        }
+
+        private void onePictureModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            mode = "single";
+            HandleShowMode();
+        }
+
+        private void slideShowModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            mode = "slideShow";
+            imageIndex = 0;
+            HandleShowMode();
+        }
+
+        private void sideNavBarAsTextListBox_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+           
+            //if the item state is selected them change the back color 
+            if ((e.State & DrawItemState.Selected) == DrawItemState.Selected)
+                e = new DrawItemEventArgs(e.Graphics,
+                                          e.Font,
+                                          e.Bounds,
+                                          e.Index,
+                                          e.State ^ DrawItemState.Selected,
+                                          e.ForeColor,
+                                          Color.White);//Choose the color
+            // Draw the background of the ListBox control for each item.
+            e.DrawBackground();
+            // Draw the current item text
+            e.Graphics.DrawString(sideNavBarAsTextListBox.Items[e.Index].ToString(), e.Font, Brushes.Black, e.Bounds, StringFormat.GenericDefault);
+            // If the ListBox has focus, draw a focus rectangle around the selected item.
+            e.DrawFocusRectangle();
+        }
+    }
+}
